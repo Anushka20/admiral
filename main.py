7f40a3c60d30aba0d2a582f1c3159f7f6eb7afa2
Anushka Tiwari <Anushkat0920@gmail.com>
2020-12-31 11:27:17 +0530

    initialisation

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+git init ---> initialisation
+git remote add origin https://github.com/Anushka20/admiral.git ------> link to the main repository
+git add .
+git commit -m message
+git push origin master---------> push to git hub specific branch
+'''
